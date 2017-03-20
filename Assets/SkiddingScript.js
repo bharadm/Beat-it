b4f570f90788658fcc23a35c4a3b52c43648b05b
blazebunny <bharadm12@gmail.com>
2017-03-21 00:37:29 +0530

    Car game without map.

---
+++ Assets/SkiddingScript.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+private var currentFrictionValue : float;
+var skidAt : float = 1.5;
+var skidSound : GameObject;
+function Start () {
+
+}
+
+function Update () {
+var hit : WheelHit;
+transform.GetComponent(WheelCollider).GetGroundHit(hit);
+currentFrictionValue= Mathf.Abs(hit.sidewaysSlip);
+if(skidAt <= currentFrictionValue){
+		Instantiate(skidSound,hit.point,Quaternion.identity);
+	}
+
+}
